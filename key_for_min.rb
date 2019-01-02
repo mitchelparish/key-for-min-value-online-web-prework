591401140e9756298216d035efd43eed489581ce
Mitchel Parish <mitchelparish@gmail.com>
2019-01-02 22:43:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,10 @@
+
 def key_for_min_value(hash)
   lowest_key = nil
   lowest_value = nil
     hash.each do |key, value|
     if lowest_value == nil || value < lowest_value
-      
+      lowest_value = value
       lowest_key = key
     end
   end
